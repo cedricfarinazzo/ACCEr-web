56996471b946c8e95e735e997674443083692896
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-03-17 22:09:47 +0100

    add download link

---
+++ WebSite/accer/class/DownloadManager.php
@@ -0,0 +1,50 @@
+<?php
+class DownloadManager {
+	
+	public $build_folder = WEB_PATH."/data/shadowminer/build/";
+	public $exe_name = NULL;
+	
+	public function __construct(PDO $db)
+	{
+		$this->setDb($db);
+	}
+	
+	public function __destruct()
+	{
+		$this->db = NULL;
+	}
+	
+	public function search()
+	{
+		try {
+			$folder_data = array_slice(scandir($this->build_folder), 2);
+			foreach($folder_data as $val) {
+				if (explode(".", $val)[count(explode(".", $val)) - 1] == "exe")
+					$this->exe_name = $val;
+					return true;
+			return false;
+			}
+		} catch(Exception $e) {
+			return false;
+		}
+		
+	}
+	
+	public function download()
+	{
+		if ($this->exe_name != NULL) {
+			$path = $this->build_folder.$this->exe_name;
+			header("Content-Description: File Transfer"); 
+			header("Content-Type: application/octet-stream"); 
+			header("Content-Disposition: attachment; filename='".$this->exe_name."'"); 
+			readfile($path);
+		}
+	}	
+	
+	//getters and setters
+	
+	public function setDb(PDO $db)
+	{
+		$this->db = $db;
+	}
+}
--- WebSite/accer/view/view_download.php
+++ WebSite/accer/view/view_download.php
@@ -3,7 +3,26 @@ $cache = new CacheManager("download");
 $in_cache = $cache->on_cache();
 if (!$in_cache) {
 	ob_start();
-
+	$downloadmanager = new DownloadManager($db);
+	$available = false;
+	if ($downloadmanager->search()) {
+		$path = $downloadmanager->build_folder;
+		$name = $downloadmanager->exe_name;
+		$size = filesize($path.$name);
+		if ($size >= 1000000000) {
+			$size = ((int)($size / 1000000000)).' Go';
+		}
+		elseif ($size >= 1000000) {
+			$size = ((int)($size / 1000000)).' mo';
+		}
+		elseif ($size >= 1000) {
+			$size = ((int)($size / 1000)).' ko';
+		}
+		else {
+			$size = ((int)$size).' o';
+		}
+		$available = true;
+	}
 	$request = ob_get_contents();
 	ob_end_clean();
 
@@ -13,11 +32,47 @@ if (!$in_cache) {
 		<div class="divider"></div>
 		<br/>
 		<div class="container">
-			Pour le moment, le jeu Shadow Miner est encore en développement.
-			La première version du jeu sortira en juin.
+			Pour le moment, le jeu Shadow Miner est encore en développement.<br/>
+			La version finale du jeu sortira en début juin.
 		</div>
 		<br/>
-		<div class="divide"></div>
+		<div class="divider"></div>
+		<?php if ($available) { ?>
+			<br/>
+			<div class="container">
+				<h4 class="center">Version disponible</h4>
+				<br/>
+				<table class="bordered highlight responsive-table">
+					<thead style="font-weight: bold;">
+						<tr>
+							<td>Nom</td>
+							<td>version</td>
+							<td>plateforme</td>
+							<td>taille</td>
+							<td>lien</td>
+						</tr>
+					</thead>
+					<tfoot style="font-weight: bold;">
+						<tr>
+							<td>Nom</td>
+							<td>version</td>
+							<td>plateforme</td>
+							<td>taille</td>
+							<td>lien</td>
+						</tr>
+					</tfoot>
+					<tbody>
+							<td><?= $name; ?></td>
+							<td>Beta(0.1)</td>
+							<td>Windows</td>
+							<td><?= $size ?></td>
+							<td><a href="<?= URL_PATH ?>/download.php">Télécharger</a></td>
+					</tbody>
+				</table>
+			</div>
+			<br/>
+			<div class="divider"></div>
+		<?php } ?>
 		<br/>
 		<div>
 			<a href="/game">Test.é</a>
